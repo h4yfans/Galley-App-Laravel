7d1e23aca7f2f6cb642adb1a3807fa3718b2ee90
Kaan Karaca <kaan94karaca@gmail.com>
2016-10-18 20:16:52 +0300

    validation added

--- resources/views/gallery/gallery.blade.php
+++ resources/views/gallery/gallery.blade.php
@@ -1,12 +1,21 @@
 @extends('master')
 
 @section('content')
+
+
+
     <div class="row">
         <div class="col-md-12">
             <h1>My Galleries</h1>
         </div>
     </div>
 
+    @if(count($errors) > 0)
+        @foreach($errors->all() as $error)
+            <li>{{$error}}</li>
+        @endforeach
+
+    @endif
 
     <div class="row">
         <div class="col-md-8">
